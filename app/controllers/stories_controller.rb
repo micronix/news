4dd46b609ea4678e5ec8c71e1c7d83f385610a80
Code the Dream <dan@unitingnc.org>
2017-04-15 15:39:44 +0000

    login system, ability to create and update stories

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,29 @@
+class StoriesController < ApplicationController
+    def index
+    end
+    
+    def new
+        @story = Story.new
+    end
+    
+    def create
+        @story = Story.new
+        @story.user_id = current_user.id
+        @story.title = params[:story][:title]
+        @story.url = params[:story][:url]
+        @story.save
+        redirect_to '/'
+    end
+    
+    def edit
+        @story = Story.find(params[:id])
+    end
+    
+    def update
+        @story = Story.find(params[:id])
+        @story.title = params[:story][:title]
+        @story.url = params[:story][:url]
+        @story.save
+        redirect_to '/'
+    end
+end
---
+++ db/migrate/20170415144914_create_stories.rb
@@ -0,0 +1,10 @@
+class CreateStories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stories do |t|
+      t.integer :user_id
+      t.string  :title
+      t.string  :url
+      t.timestamps
+    end
+  end
+end
